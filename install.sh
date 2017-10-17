4df4a91bfd43c434d3fcd1153327ad1c43d1235c
Wangxy <xtwxy@hotmail.com>
2017-10-17 17:33:34 +0800

    Problem: https://github.com/wangdy123/myDCIM/issues/2fixed none portable way of executing shell commands.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 PRG=$(readlink -f $0)
-PRG_DIR=`dirname "$PRG"`
+PRG_DIR=$(dirname "$PRG")
 
-MODULES=`find ./lib -maxdepth 1 -type d`
+MODULES=$(find ./lib -maxdepth 1 -type d)
 for d in $MODULES; do
   if [ -f $d/package.json ] ;then
   echo "install $d"
